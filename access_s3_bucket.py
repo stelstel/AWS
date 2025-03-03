31bb0b8beefaf78e637b0c73babb395a011b07f7
Steve <sthlm_32@hotmail.com>
2025-03-03 09:33:17 +0100

    First commit

---
+++ access_s3_bucket.py
@@ -0,0 +1,24 @@
+import boto3
+
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError
+
+def list_bucket_objects(bucket_name):
+    """List objects in an S3 bucket"""
+    s3 = boto3.client('s3')
+
+    try:
+        response = s3.list_objects_v2(Bucket=bucket_name)
+
+        if 'Contents' in response:
+            for obj in response['Contents']:
+                print(f' - {obj["Key"]}')
+        else:
+            print(f'Bucket {bucket_name} is empty.')
+    except NoCredentialsError:
+        print('Credentials not available.')
+    except PartialCredentialsError:
+        print('Incomplete credentials provided.')
+if __name__ == '__main__':
+    bucket_name = 'stelstelbucket'
+
+list_bucket_objects(bucket_name)
